4dcb4dd339e7e967712d02484eb72bbf586275db
Orwa400 <matheworwa3@gmail.com>
2023-10-02 16:16:37 +0300

    Initialize bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from .config import DevConfig
 from app import views
 from app import error
+from flask_bootstrap import Bootstrap
 
 # Initializing application
 app = Flask(__name__,instance_relative_config= True)
@@ -9,5 +10,8 @@ app = Flask(__name__,instance_relative_config= True)
 # Setting up configuration
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
+# Initializing Flask Extensions
+bootsrap = Bootstrap(app)
 
 
